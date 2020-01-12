bd9a19a696327e551682721a9f58aa8a5e71b49b
Vaniri <foxifennec@gmail.com>
2020-01-11 22:55:13 -0500

    added project video

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -51,29 +51,3 @@ $(".scrollable-link").on("click", function () {
   event.preventDefault();
 });
 
-//slideShow portfolio images
-
-let slideIndex = [1, 1, 1, 1];
-let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4"]
-showSlides(1, 0);
-showSlides(1, 1);
-showSlides(1, 2);
-showSlides(1, 3);
-
-function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
-}
-
-function showSlides(n, no) {
-  let i;
-  let x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) { slideIndex[no] = 1 }
-  if (n < 1) { slideIndex[no] = x.length }
-
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-
-  x[slideIndex[no] - 1].style.display = "block";
-}
-
